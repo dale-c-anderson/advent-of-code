f88b4f03051eb4357fbc9c5431eb50fb96fde4a1
Dale Anderson <danderson@acromedia.com>
2022-12-03 21:27:45 -0800

    Complete part 2

---
+++ 2022/04/part2.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""
+Advent of Code (AoC) boilerplate script for daily puzzles
+"""
+
+__author__ = "Dale Anderson"
+__version__ = "0.1.0"
+__license__ = "GPLv3"
+
+import argparse
+import logging
+import sys
+
+
+def main():
+    handle = open("./input")
+    sa_pairs = list(filter(None, handle.read().split('\n')))
+    count = 0
+    for pair in sa_pairs:
+        sa_elf1, sa_elf2 = pair.split(',')
+        log.debug(f"sa_elf1: {sa_elf1}, sa_elf2: {sa_elf2}")
+        if overlaps(sa_elf1, sa_elf2) or overlaps(sa_elf2, sa_elf1):
+            print("overlaps")
+            count += 1
+        else:
+            print("no overlap")
+
+    print(f"part1 answer: {count}")
+
+
+def overlaps(a, b):
+    a_lb, a_ub = a.split('-')
+    b_lb, b_ub = b.split('-')
+    return int(a_lb) <= int(b_lb) <= int(a_ub) or int(a_lb) <= int(b_ub) <= int(a_ub)
+
+
+def fully_contains(haystack, needle):
+    haystack_lb, haystack_ub = haystack.split('-')
+    needle_lb, needle_ub = needle.split('-')
+    return int(haystack_lb) <= int(needle_lb) and int(haystack_ub) >= int(needle_ub)
+
+
+def span_to_range(val):
+    a, b = val.split('-')
+    return range(int(a), int(b))
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=1,
+        help="Verbosity (-v, -vv, etc)")
+    args = parser.parse_args()
+
+    # Let terminal operator control the logging level sent to stderr
+    levels = [
+        logging.WARNING,
+        logging.INFO,
+        logging.DEBUG,
+    ]
+    level = levels[min(args.verbose, len(levels) - 1)]
+
+    ##################################
+    # DO NOT SET basicConfig.
+    # Since we have handlers with individual log levels, using basicConfig will ruin everything.
+    ###################################
+    # logging.basicConfig()
+    #     datefmt='%Y-%m-%d %H:%M:%S',
+    # )
+
+    default_date_format = '%Y-%m-%d %H:%M:%S'
+    default_log_format = '%(asctime)s.%(msecs)03d %(levelname)s %(module)s/%(funcName)s(): %(message)s'
+
+    # Create our own custom logging configuration. Don't use the built-in automagic stuff.
+    log = logging.getLogger("foo")
+
+    # Set the minimum threshold needed by any handler, then override desired levels for each handler.
+    # If this is set to "CRITICAL", for instance, nothing will ever appear in any log handler.
+    log.setLevel(logging.DEBUG)
+
+    # Set up handler for terminal logging
+    console_handler = logging.StreamHandler(sys.stderr)
+    console_handler.setLevel(level)  # Let terminal log level be controlled by args
+    console_handler.setFormatter(logging.Formatter(default_log_format, datefmt=default_date_format))
+    log.addHandler(console_handler)
+
+    main()
