fe12d0a2da9e6f9fb29de2b33908f9d321cb8585
Dale Anderson <danderson@acromedia.com>
2022-12-03 21:32:15 -0800

    Clean up

--- 2022/04/part1.py
+++ 2022/04/part1.py
@@ -1,16 +1,4 @@
 #!/usr/bin/env python3
-"""
-Advent of Code (AoC) boilerplate script for daily puzzles
-"""
-
-__author__ = "Dale Anderson"
-__version__ = "0.1.0"
-__license__ = "GPLv3"
-
-import argparse
-import logging
-import sys
-
 
 def main():
     handle = open("./input")
@@ -18,14 +6,12 @@ def main():
     count = 0
     for pair in sa_pairs:
         sa_elf1, sa_elf2 = pair.split(',')
-        log.debug(f"sa_elf1: {sa_elf1}, sa_elf2: {sa_elf2}")
         if fully_contains(sa_elf1, sa_elf2) or fully_contains(sa_elf2, sa_elf1):
             print("fully contained")
             count += 1
         else:
             print("not fully contained")
-
-    print(f"part1 answer: {count}")
+    print(f"answer: {count}")
 
 
 def fully_contains(haystack, needle):
@@ -34,51 +20,5 @@ def fully_contains(haystack, needle):
     return int(haystack_lb) <= int(needle_lb) and int(haystack_ub) >= int(needle_ub)
 
 
-def span_to_range(val):
-    a, b = val.split('-')
-    return range(int(a), int(b))
-
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="count",
-        default=1,
-        help="Verbosity (-v, -vv, etc)")
-    args = parser.parse_args()
-
-    # Let terminal operator control the logging level sent to stderr
-    levels = [
-        logging.WARNING,
-        logging.INFO,
-        logging.DEBUG,
-    ]
-    level = levels[min(args.verbose, len(levels) - 1)]
-
-    ##################################
-    # DO NOT SET basicConfig.
-    # Since we have handlers with individual log levels, using basicConfig will ruin everything.
-    ###################################
-    # logging.basicConfig()
-    #     datefmt='%Y-%m-%d %H:%M:%S',
-    # )
-
-    default_date_format = '%Y-%m-%d %H:%M:%S'
-    default_log_format = '%(asctime)s.%(msecs)03d %(levelname)s %(module)s/%(funcName)s(): %(message)s'
-
-    # Create our own custom logging configuration. Don't use the built-in automagic stuff.
-    log = logging.getLogger("foo")
-
-    # Set the minimum threshold needed by any handler, then override desired levels for each handler.
-    # If this is set to "CRITICAL", for instance, nothing will ever appear in any log handler.
-    log.setLevel(logging.DEBUG)
-
-    # Set up handler for terminal logging
-    console_handler = logging.StreamHandler(sys.stderr)
-    console_handler.setLevel(level)  # Let terminal log level be controlled by args
-    console_handler.setFormatter(logging.Formatter(default_log_format, datefmt=default_date_format))
-    log.addHandler(console_handler)
-
     main()
