ea26307fbd9e6cbfb57d38ea0b85f4107b3db363
Dale Anderson <danderson@acromedia.com>
2022-12-05 21:27:28 -0800

    Tidy up

--- 2022/06/solution.py
+++ 2022/06/solution.py
@@ -1,111 +1,39 @@
 #!/usr/bin/env python3
 """
-Advent of Code (AoC) , Day 06
+Advent of Code (AoC) 2022, Day 06
 """
 
 __author__ = "Dale Anderson"
-
-import argparse
-import logging
-import sys
 
 
 def main():
     handle = open("./input")
     data = handle.read()
 
-    part1_answer = part1(data)
+    part1_answer = solution(data, buffer_size=4)
     print(f'Part 1: {part1_answer}')
 
-    part2_answer = part2(data)
+    part2_answer = solution(data, buffer_size=14)
     print(f'Part 2: {part2_answer}')
 
 
-def part1(data):
-    bufsize = 4
+def solution(data, buffer_size):
     buff = []
     count = 0
     for char in data:
-        log.debug(f'char: {char}')
         count += 1
         buff.append(char)
-        log.debug(buff)
-        if len(buff) > bufsize:
+        if len(buff) > buffer_size:
             buff.pop(0)
-        #log.debug(f'buff after pop: {buff}')
-        if len(buff) >= bufsize:
-            uniq_check = all_unique(buff)
-            #log.debug(f'uniq_check: {uniq_check}')
-            if uniq_check:
-               break
+        if len(buff) >= buffer_size:
+            if all_unique(buff):
+                break
     return count
 
+
 def all_unique(buff):
-    #log.debug(f'all uniq? buff: {buff}')
     return len(set(buff)) == len(buff)
 
 
-def part2(data):
-    bufsize = 14
-    buff = []
-    count = 0
-    for char in data:
-        log.debug(f'char: {char}')
-        count += 1
-        buff.append(char)
-        log.debug(buff)
-        if len(buff) > bufsize:
-            buff.pop(0)
-        #log.debug(f'buff after pop: {buff}')
-        if len(buff) >= bufsize:
-            uniq_check = all_unique(buff)
-            #log.debug(f'uniq_check: {uniq_check}')
-            if uniq_check:
-               break
-    return count
-
-
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="count",
-        default=1,
-        help="Verbosity (-v, -vv, etc)")
-    args = parser.parse_args()
-
-    # Let terminal operator control the logging level sent to stderr
-    levels = [
-        logging.WARNING,
-        logging.INFO,
-        logging.DEBUG,
-    ]
-    level = levels[min(args.verbose, len(levels) - 1)]
-
-    ##################################
-    # DO NOT SET basicConfig.
-    # Since we have handlers with individual log levels, using basicConfig will ruin everything.
-    ###################################
-    # logging.basicConfig()
-    #     datefmt='%Y-%m-%d %H:%M:%S',
-    # )
-
-    default_date_format = '%Y-%m-%d %H:%M:%S'
-    default_log_format = '%(asctime)s.%(msecs)03d %(levelname)s %(module)s/%(funcName)s(): %(message)s'
-
-    # Create our own custom logging configuration. Don't use the built-in automagic stuff.
-    log = logging.getLogger("foo")
-
-    # Set the minimum threshold needed by any handler, then override desired levels for each handler.
-    # If this is set to "CRITICAL", for instance, nothing will ever appear in any log handler.
-    log.setLevel(logging.DEBUG)
-
-    # Set up handler for terminal logging
-    console_handler = logging.StreamHandler(sys.stderr)
-    console_handler.setLevel(level)  # Let terminal log level be controlled by args
-    console_handler.setFormatter(logging.Formatter(default_log_format, datefmt=default_date_format))
-    log.addHandler(console_handler)
-
     main()
