cd5e9177a1e54cbeee856eb8560ec4fc89b70fd7
Dale Anderson <danderson@acromedia.com>
2022-12-05 21:00:21 -0800

    Start Day 6 part 1

---
+++ 2022/06/solution.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""
+Advent of Code (AoC) , Day 06
+"""
+
+__author__ = "Dale Anderson"
+
+import argparse
+import logging
+import sys
+
+
+def main():
+    handle = open("./input")
+    data = handle.read()
+
+    part1_answer = part1(data)
+    print(f'Part 1: {part1_answer}')
+
+    part2_answer = part2(data)
+    print(f'Part 2: {part2_answer}')
+
+
+def part1(data):
+    return 0
+
+
+def part2(data):
+    return 0
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=1,
+        help="Verbosity (-v, -vv, etc)")
+    args = parser.parse_args()
+
+    # Let terminal operator control the logging level sent to stderr
+    levels = [
+        logging.WARNING,
+        logging.INFO,
+        logging.DEBUG,
+    ]
+    level = levels[min(args.verbose, len(levels) - 1)]
+
+    ##################################
+    # DO NOT SET basicConfig.
+    # Since we have handlers with individual log levels, using basicConfig will ruin everything.
+    ###################################
+    # logging.basicConfig()
+    #     datefmt='%Y-%m-%d %H:%M:%S',
+    # )
+
+    default_date_format = '%Y-%m-%d %H:%M:%S'
+    default_log_format = '%(asctime)s.%(msecs)03d %(levelname)s %(module)s/%(funcName)s(): %(message)s'
+
+    # Create our own custom logging configuration. Don't use the built-in automagic stuff.
+    log = logging.getLogger("foo")
+
+    # Set the minimum threshold needed by any handler, then override desired levels for each handler.
+    # If this is set to "CRITICAL", for instance, nothing will ever appear in any log handler.
+    log.setLevel(logging.DEBUG)
+
+    # Set up handler for terminal logging
+    console_handler = logging.StreamHandler(sys.stderr)
+    console_handler.setLevel(level)  # Let terminal log level be controlled by args
+    console_handler.setFormatter(logging.Formatter(default_log_format, datefmt=default_date_format))
+    log.addHandler(console_handler)
+
+    main()
