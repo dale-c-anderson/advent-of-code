ef460945f61884e19254bbfd6930f6739f4c33a3
Dale Anderson <danderson@acromedia.com>
2022-12-07 01:18:00 -0800

    Fix several bugs, finally get right answer for part 1

--- 2022/07/solution.py
+++ 2022/07/solution.py
@@ -8,6 +8,7 @@ __author__ = "Dale Anderson"
 import argparse
 import logging
 import sys
+from pathlib import Path
 
 
 def main():
@@ -28,57 +29,65 @@ def part1(transcript_raw):
     current_dir = '/'
     transcript = transcript_raw.split('\n')
     for command in transcript:
-        log.debug(f'dir_data: {dir_data}')
+        # log.debug(f'dir_data: {dir_data}')
         parts = command.split(' ')
         if parts[0] == '':
             log.debug(f'parts 0 is an empty line.')
+            pass
         elif parts[0].isnumeric():
             my_bytes = int(parts[0])
-            log.debug(f'Is numeric. Added {my_bytes} bytes to {current_dir}')
+            log.debug(f'{current_dir} + {my_bytes} bytes')
             dir_data[current_dir] += my_bytes
             # bytes_now = sum(dir_data.values())
             # if bytes_now > bytes_limit:
             #     return bytes_now
 
         elif parts[0] == 'dir':
-            log.debug(f'parts 0 Is dir')
             dir_name = parts[1]
-            log.debug(f'dir {dir_name}')
+            if current_dir == '/':
+                log.debug(f'/{dir_name}')
+            else:
+                log.debug(f'{current_dir}/{dir_name}')
+            # log.debug(f'dir {dir_name}')
             pass # There is nothing to process here.
 
         elif parts[0] == '$':
             command = parts[1]
-            log.debug(f'parts 0 Is $, command is {command}')
             if command == 'ls':
-                log.debug('ls')
+                log.debug(f'{command} {current_dir}')
                 pass  # There's nothing to process here.
 
             elif command == 'cd':
                 dir_name = parts[2]
                 if dir_name == '/':
-                    log.debug('cd /')
+                    log.debug('cd / (real root)')
                     pass # There's nothing to process here.
                 elif dir_name == '..':
-                    log.debug(f'cd .. current_dir before {current_dir}')
-                    current_dir = current_dir.split('/').pop().join('/')
-                    log.debug(f'cd .. current_dir after {current_dir}')
+                    before = current_dir
+                    parent_dir = str(Path(before).parent)
+                    # log.debug(f'cd .. current_dir before {current_dir}')
+                    current_dir = parent_dir
+                    log.debug(f'cd {current_dir} (calculated). Before: {before}. Parent: {parent_dir}')
                 else:
                     # Must be the name of a directory! "cd" into it.
                     if current_dir == '/':
                         current_dir = f'/{dir_name}'
                     else:
                         current_dir = f'{current_dir}/{dir_name}'
-                    dir_data[f'{current_dir}'] = 0
-                    log.debug(f'cd\'ing into {dir_name} - new current dir = {current_dir}')
+                    if current_dir in dir_data:
+                        # log.warning(f"Unexpected current dir already exists: {current_dir}")
+                        pass
+                    else:
+                        dir_data[current_dir] = 0
+                    log.debug(f'cd {current_dir}')
             else:
                 raise ValueError('Unhandled command')
         else:
             raise ValueError('Unhandled Parts[0] condition')
 
-    # Show dirs before we sum totals
+    # # Show dirs before we sum totals
     for dir_name, dir_bytes in dir_data.items():
         print(f'BEFORE {dir_bytes}\t{dir_name}\t')
-
     print('')
 
     # Sum up all subdirectory bytes
@@ -93,13 +102,13 @@ def part1(transcript_raw):
                 combined_total += int(match_dir_bytes)
         #print('')
         if match_count > 1:
-            dir_data[dir_name] =  combined_total
+          dir_data[dir_name] = combined_total
         # if f'dir_name' in dir_data.keys():
         #     print(f'FOUND   {dir_bytes}\t{dir_name}\t')
         # else:
         #     print(f'not     {dir_bytes}\t{dir_name}\t')
 
-
+    #
     print('')
     for dir_name, dir_bytes in dir_data.items():
         print(f'AFTER {dir_bytes}\t{dir_name}\t')
@@ -109,13 +118,13 @@ def part1(transcript_raw):
     bytes_limit = 100000
     total = 0
     for dir_name, dir_bytes in dir_data.items():
-        if dir_bytes <= 100000:
-            total += dir_bytes
+        if int(dir_bytes) <= bytes_limit:
+            total += int(dir_bytes)
             print(f'INCLUDE {dir_bytes}\t{dir_name}\t')
         else:
-            print(f'exclude {dir_bytes}\t{dir_name}\t')
+            print(f'        {dir_bytes}\t{dir_name}\t')
 
-    print(f'total: {total}')
+    return total
 
 
 def part2(data):
