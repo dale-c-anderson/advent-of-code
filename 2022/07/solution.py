a206fe5d44f39bb52536edaae522d2f06930e7b6
Dale Anderson <danderson@acromedia.com>
2022-12-06 23:56:37 -0800

    Submit wrong answer

--- 2022/07/solution.py
+++ 2022/07/solution.py
@@ -21,10 +21,71 @@ def main():
     print(f'Part 2: {part2_answer}')
 
 
-def part1(data):
-    return 0
+def part1(transcript_raw):
+    dir_data = {
+        '/': 0
+    }
+    current_dir = '/'
+    transcript = transcript_raw.split('\n')
+    for command in transcript:
+        log.debug(f'dir_data: {dir_data}')
+        parts = command.split(' ')
+        if parts[0] == '':
+            log.debug(f'parts 0 is an empty line.')
+        elif parts[0].isnumeric():
+            my_bytes = int(parts[0])
+            log.debug(f'Is numeric. Added {my_bytes} bytes to {current_dir}')
+            dir_data[current_dir] += my_bytes
+            # bytes_now = sum(dir_data.values())
+            # if bytes_now > bytes_limit:
+            #     return bytes_now
+
+        elif parts[0] == 'dir':
+            log.debug(f'parts 0 Is dir')
+            dir_name = parts[1]
+            log.debug(f'dir {dir_name}')
+            pass # There is nothing to process here.
+
+        elif parts[0] == '$':
+            command = parts[1]
+            log.debug(f'parts 0 Is $, command is {command}')
+            if command == 'ls':
+                log.debug('ls')
+                pass  # There's nothing to process here.
+
+            elif command == 'cd':
+                dir_name = parts[2]
+                if dir_name == '/':
+                    log.debug('cd /')
+                    pass # There's nothing to process here.
+                elif dir_name == '..':
+                    log.debug(f'cd .. current_dir before {current_dir}')
+                    current_dir = current_dir.split('/').pop().join('/')
+                    log.debug(f'cd .. current_dir after {current_dir}')
+                else:
+                    # Must be the name of a directory! "cd" into it.
+                    if current_dir == '/':
+                        current_dir = f'/{dir_name}'
+                    else:
+                        current_dir = f'{current_dir}/{dir_name}'
+                    dir_data[f'{current_dir}'] = 0
+                    log.debug(f'cd\'ing into {dir_name} - new current dir = {current_dir}')
+            else:
+                raise ValueError('Unhandled command')
+        else:
+            raise ValueError('Unhandled Parts[0] condition')
 
 
+    bytes_limit = 100000
+    total = 0
+    for dir_name, dir_bytes in dir_data.items():
+        if total + dir_bytes <= 100000:
+            total += dir_bytes
+            print(f'INCLUDE {dir_bytes}\t{dir_name}\t')
+        else:
+            print(f'exclude {dir_bytes}\t{dir_name}\t')
+
+    print(f'total: {total}')
 def part2(data):
     return 0
 
