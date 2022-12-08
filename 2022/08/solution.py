a13b75bb600e45428e9db58c22542e3015e62f38
Dale Anderson <danderson@acromedia.com>
2022-12-07 23:19:58 -0800

    Clone part 1 as starting point for part 2

--- 2022/08/solution.py
+++ 2022/08/solution.py
@@ -15,8 +15,6 @@ def main():
     data = handle.read().split('\n')
     data = list(filter(None, data))  # get rid of empty rows
 
-    #log.debug(data)
-
     part1_answer = part1(data)
     print(f'Part 1: {part1_answer}')
 
@@ -75,7 +73,54 @@ def part1(data):
     return visible_trees
 
 def part2(data):
-    return 0
+    col_count = len(data[0])
+    columns = [None] * col_count
+
+    # Populate column arrays
+    for row_index, line in enumerate(data):
+        for col_index, char in enumerate(line):
+            if row_index == 0:
+                columns[col_index] = []
+            columns[col_index].append(char)
+
+    # Calculate visibility
+    visible_trees = 0
+    for row_index, row_as_string in enumerate(data):
+        row_as_list = list(row_as_string)
+        for col_index, char in enumerate(row_as_string):
+            height = int(char)
+
+
+            trees_on_top = columns[col_index][0:row_index]
+            log.debug(f'f trees_on_top {trees_on_top}')
+
+            trees_to_left = row_as_list[0:col_index]
+            log.debug(f' trees_to_left {trees_to_left}')
+
+            log.debug(f'height {height}')
+
+            trees_to_right = row_as_list[col_index + 1:]
+            log.debug(f'f trees_to_right {trees_to_right}')
+
+            trees_on_bottom = columns[col_index][row_index + 1:]
+            log.debug(f'f trees_on_bottom {trees_on_bottom}')
+
+            if all(height > int(x) for x in trees_to_left) \
+                    or all(height > int(x) for x in trees_to_right) \
+                    or all(height > int(x) for x in trees_on_top) \
+                    or all(height > int(x) for x in trees_on_bottom):
+                visible_trees += 1
+                log.debug(f'tree at {row_index},{col_index} is visible')
+            else:
+                log.debug(f'tree at {row_index},{col_index} is hidden')
+
+            log.debug('')
+
+        log.debug('')
+        log.debug(f'--- new row. Visible trees so far: {visible_trees} --- ')
+        log.debug('')
+
+    return visible_trees
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
