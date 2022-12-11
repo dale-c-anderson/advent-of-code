a645f3496fd4eaf9d34c089968887ffb77a84360
Dale Anderson <danderson@acromedia.com>
2022-12-11 11:46:19 -0800

    Clone day10 part1 as part2 starting point

--- 2022/10/solution.py
+++ 2022/10/solution.py
@@ -66,7 +66,47 @@ def part1(data1):
 
 
 def part2(data2):
-    return data2
+    register_value = 1
+    op_costs = {
+        'noop': 1,
+        'addx': 2,
+    }
+    cycles_left = 0
+    cycle_number = -1
+    op = None
+    signal_strength_sum = 0
+    op_value = 0
+    while cycles_left > 0 or len(data1) > 0:
+        cycle_number += 1
+        signal_strength = register_value * cycle_number
+        log.debug(f'begin cycle {cycle_number}, left {cycles_left}, op {op}, opval {op_value}'
+                  f', register_value {register_value}, signal_strength {signal_strength}'
+                  f', signal_strength_sum {signal_strength_sum}, len(data) {len(data1)}'
+                  )
+        if ((cycle_number -20) % 40 == 0 and cycle_number > 40) or cycle_number == 20:
+            log.info(f'------- adding {signal_strength} to {signal_strength_sum} at cycle {cycle_number}')
+            signal_strength_sum += signal_strength
+            log.info(f'------- new sum {signal_strength_sum}')
+        if cycles_left > 1:
+            cycles_left -= 1
+            continue
+        else:
+            if op == 'addx':
+                log.info(f'           adding {op_value} to {register_value} at cycle {cycle_number}')
+                register_value += int(op_value)
+                log.info(f'           new register_value {register_value}')
+            if len(data1) == 0:
+                break
+            instruction = data1.pop(0)
+            if ' ' in instruction:
+                op, op_value = instruction.split(' ')
+                log.debug(f'op: {op}, op_value: {op_value}')
+            else:
+                op = instruction
+                log.debug(f'op: {op}')
+            cycles_left = op_costs[op]
+
+    return signal_strength_sum
 
 
 if __name__ == "__main__":
