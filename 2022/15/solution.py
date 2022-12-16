91f0169c6d9e5b3e0a9d5d8f616fc7f4649a9e58
Dale Anderson <danderson@acromedia.com>
2022-12-15 20:53:29 -0800

    Save day 15 wip; test works, but real input causes script to crash.

--- 2022/15/solution.py
+++ 2022/15/solution.py
@@ -6,9 +6,8 @@ AdventOfCode.com 2022, Day 15
 __author__ = "Dale Anderson"
 
 import argparse
-import logging
+# import logging
 import sys
-
 
 def main(data0):
 
@@ -24,8 +23,65 @@ def main(data0):
 
 
 def part1(data):
-    return 0
+    sensors = set()
+    beacons = set()
+    cleared = set()
+    map = []
+    for line in data:
+        words = line.split()
+        # 0      1    2    3    4       5     6  7   8    9
+        # Sensor at x=8, y=7: closest beacon is at x=2, y=10
+        sx = int(words[2].replace('x=', '').replace(',', ''))
+        sy = int(words[3].replace('y=', '').replace(':', ''))
+        bx = int(words[8].replace('x=', '').replace(',', ''))
+        by = int(words[9].replace('y=', ''))
+        sensors.add((sx, sy))
+        beacons.add((bx, by))
+        map.append((sx, sy, bx, by))
+        taxicab_distance = abs(sx - bx) + abs(sy - by)
+        for yi, y in enumerate(range(sy - taxicab_distance, sy + taxicab_distance + 1)):
+            # log.debug(f'yi: {yi}, y: {y}')
+            if yi <= taxicab_distance:
+                xlower = sx - yi
+                xupper = sx + yi + 1
+            else:
+                xlower = sx - (2 * taxicab_distance - yi)
+                xupper = sx + (2 * taxicab_distance - yi) + 1
+            for x in range(xlower, xupper):
+                cleared.add((x, y))
+        # log.debug(f'sensor: {sx}, {sy}, beacon: {bx}, {by}, cab distance: {taxicab_distance}')
 
+    # draw_cleared(sensors, beacons, cleared, 'cleared')
+    count = 0
+    for x, y in cleared:
+        if y == 2000000:
+            if not (x, y) in sensors:
+                if not (x, y) in beacons:
+                    count += 1
+    return count
+
+
+def draw_cleared(sensors, beacons, cleared, title):
+    # if not at_least_log_level_info():
+    #     return
+    # log.handlers[0].flush()
+    print(f'\n--------- {title} ---------\n')
+    x_min = min([x for x, y in cleared])
+    x_max = max([x for x, y in cleared])
+    y_min = min([y for x, y in cleared])
+    y_max = max([y for x, y in cleared])
+    # log.debug(f'x_min: {x_min}, x_max: {x_max}, y_min: {y_min}, y_max: {y_max}')
+    for y in range(y_min -1, y_max +1):
+        for x in range(x_min - 1, x_max + 1):
+            if (x, y) in sensors:
+                print('S', end='')
+            elif (x, y) in beacons:
+                print('B', end='')
+            elif (x, y) in cleared:
+                print('#', end='')
+            else:
+                print(' ', end='')
+        print(f' {y}')
 
 def part2(data):
     return 0
@@ -49,25 +105,25 @@ if __name__ == "__main__":
         data = open("./input").read()
 
     # Support verbosity level
-    levels = [
-        logging.WARNING,
-        logging.INFO,
-        logging.DEBUG,
-    ]
-    level = levels[min(args.verbose, len(levels) - 1)]
+    # levels = [
+    #     logging.WARNING,
+    #     logging.INFO,
+    #     logging.DEBUG,
+    # ]
+    # level = levels[min(args.verbose, len(levels) - 1)]
 
     # A named logger lets us add more handlers if/when needed.
-    log = logging.getLogger("foo")
+    # log = logging.getLogger("foo")
 
     # Set the minimum threshold needed by any handler, then override desired levels for each handler.
     # If this is set to "CRITICAL", for instance, nothing will ever appear in any log handler.
-    log.setLevel(logging.DEBUG)
+    # log.setLevel(logging.DEBUG)
 
     # Set up handler for terminal logging
-    console_handler = logging.StreamHandler(sys.stderr)
-    console_handler.setLevel(level)  # Let terminal log level be controlled by args
-    default_log_format = '%(levelname)s %(funcName)s(): %(message)s'
-    console_handler.setFormatter(logging.Formatter(default_log_format))
-    log.addHandler(console_handler)
+    # console_handler = logging.StreamHandler(sys.stderr)
+    # console_handler.setLevel(level)  # Let terminal log level be controlled by args
+    # default_log_format = '%(levelname)s %(funcName)s(): %(message)s'
+    # console_handler.setFormatter(logging.Formatter(default_log_format))
+    # log.addHandler(console_handler)
 
     main(data)
