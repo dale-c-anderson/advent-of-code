26f59b7bdf179f2e58ccf754d2622b1f2a83027a
Dale Anderson <danderson@acromedia.com>
2023-12-02 00:13:34 -0800

    Commit failed attempt N

--- 2023/01/solution.py
+++ 2023/01/solution.py
@@ -55,27 +55,90 @@ def part2(data):
     # zoneight234
     # 7pqrstsixteen
 
-
-    # Step 1 - replace all english numbers with digits
-
-    new_data = []
+    first_number_words_replaced = []
     for line in data:
-        new_line = line
-        new_line = new_line.replace('one', '1')
-        new_line = new_line.replace('two', '2')
-        new_line = new_line.replace('three', '3')
-        new_line = new_line.replace('four', '4')
-        new_line = new_line.replace('five', '5')
-        new_line = new_line.replace('six', '6')
-        new_line = new_line.replace('seven', '7')
-        new_line = new_line.replace('eight', '8')
-        new_line = new_line.replace('nine', '9')
-        new_line = new_line.replace('zero', '0')
-        new_data.append(new_line)
-        print(f"new_line: {new_line}")
+        # crawl the string, and replace the first english whole-word number with a digit
+        # FIRST WORDS ONLY. We'll do the same thing from the back of the string next.
+        forward_line = ''
+        first_word_has_been_replaced = False
+        for char in line:
+            forward_line += char
+            if not first_word_has_been_replaced:
+                if string_contains_number_word(forward_line):
+                    forward_line = replace_word_number_with_digit(forward_line)
+                    first_word_has_been_replaced = True
 
-    new_sum = part1(new_data)
+        log.info(f"orig 1\t{line}\treplacement_1\t{forward_line}")
+        first_number_words_replaced.append(forward_line)
+
+
+    # Now repeat the above process but from the back of the string.
+    last_words_replaced = []
+    for line in first_number_words_replaced:
+        # crawl the string from the back, and replace the first english whole-word number with a digit
+        new_string = ''
+        did_replacement = False
+        log.info('\n\n----\n\n')
+        for char in flip(line):
+            new_string = new_string + char
+            log.debug(f"new_string: {new_string}")
+            if not did_replacement:
+                check = flip(new_string)
+                log.debug(f"check: {check}")
+                if string_contains_number_word(check):
+                    replaced = replace_word_number_with_digit(check)
+                    log.debug(f"replaced: {replaced}")
+                    did_replacement = True
+                    new_string = flip(replaced)
+                    log.debug(f"new_string: {new_string}")
+        backward_finished = new_string[::-1]
+        log.info(f"orig 2\t{line}\tbackward_finished\t{backward_finished}")
+        last_words_replaced.append(backward_finished)
+
+
+    new_sum = part1(last_words_replaced)
+
     return new_sum
+
+
+def flip(line):
+    return line[::-1]
+
+
+def string_contains_number_word(haystack):
+    if 'one' in haystack:
+        return True
+    if 'two' in haystack:
+        return True
+    if 'three' in haystack:
+        return True
+    if 'four' in haystack:
+        return True
+    if 'five' in haystack:
+        return True
+    if 'six' in haystack:
+        return True
+    if 'seven' in haystack:
+        return True
+    if 'eight' in haystack:
+        return True
+    if 'nine' in haystack:
+        return True
+    return False
+
+
+def replace_word_number_with_digit(text):
+    text = text.replace('one', '1')
+    text = text.replace('two', '2')
+    text = text.replace('three', '3')
+    text = text.replace('four', '4')
+    text = text.replace('five', '5')
+    text = text.replace('six', '6')
+    text = text.replace('seven', '7')
+    text = text.replace('eight', '8')
+    text = text.replace('nine', '9')
+    return text
+
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
